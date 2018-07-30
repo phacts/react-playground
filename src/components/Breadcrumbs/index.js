b2467c9ffadf2104f0b6d6c73c48ec5ed58d0cb8
Steve Grimm <phacts@gmail.com>
2018-07-29 22:50:04 -0400

    Formatted breadcrumbs using semantic.

--- src/components/Breadcrumbs/index.js
+++ src/components/Breadcrumbs/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Breadcrumb } from 'semantic-ui-react';
 
 // Functional component, just a function that returns a react JSX element (using the ())
 const Breadcrumbs = props => (
-  <Link to='/'>
-    Back to Search {
-      !!props.linkTitle
-      &&
-      "(" + props.linkTitle + ")"
-      }
-  </Link>
+  
+  <Breadcrumb>
+    <Breadcrumb.Section>
+      <Link to='/'>
+      
+        Back to Search {
+          !!props.linkTitle
+          &&
+          "(" + props.linkTitle + ")"
+          }
+      </Link>
+    </Breadcrumb.Section>
+  </Breadcrumb>
 )
 
 export default Breadcrumbs;
